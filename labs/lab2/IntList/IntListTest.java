38a3c86bc581ae72bca83a433b9d787930b514d0
boyuangong <astergong@gmail.com>
2017-02-12 15:37:35 -0600

    add hw1 lab1 lab2

---
+++ labs/lab2/IntList/IntListTest.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IntListTest {
+
+    /** Example test that verifies correctness of the IntList.list static 
+     *  method. The main point of this is to convince you that 
+     *  assertEquals knows how to handle IntLists just fine.
+     */
+
+    @Test 
+    public void testList() {
+        IntList one = new IntList(1, null);
+        IntList twoOne = new IntList(2, one);
+        IntList threeTwoOne = new IntList(3, twoOne);
+
+        IntList x = IntList.list(3, 2, 1);
+        assertEquals(threeTwoOne, x);
+    }
+
+    @Test
+    public void testdSquareList() {
+      IntList L = IntList.list(1, 2, 3);
+      IntList.dSquareList(L);
+      assertEquals(IntList.list(1, 4, 9), L);
+    }
+
+    /** Do not use the new keyword in your tests. You can create
+     *  lists using the handy IntList.list method.  
+     * 
+     *  Make sure to include test cases involving lists of various sizes
+     *  on both sides of the operation. That includes the empty list, which
+     *  can be instantiated, for example, with 
+     *  IntList empty = IntList.list(). 
+     *
+     *  Keep in mind that dcatenate(A, B) is NOT required to leave A untouched.
+     *  Anything can happen to A. 
+     */
+
+    //TODO:  Create testSquareListRecursive()
+	@Test
+	public void testSquareListRecursive(){
+	  IntList L = IntList.list(1, 2, 3);
+	  IntList C = IntList.squareListRecursive(L);
+	  assertEquals(IntList.list(1, 4, 9), C);
+	}
+    //TODO:  Create testDcatenate and testCatenate
+	@Test
+	public void testDcatenate(){
+	  IntList A = IntList.list(1, 2, 3);
+	  IntList B = IntList.list(4, 5, 6);
+	  IntList C = IntList.list(1, 2, 3, 4, 5, 6);
+	  assertEquals(IntList.dcatenate(A, B), C);
+
+	}	
+	@Test
+	public void testcatenate(){
+	  IntList D;
+	  IntList E;
+	  IntList A = D = IntList.list(1, 2, 3);
+	  IntList B = E = IntList.list(4, 5, 6);
+	  IntList C = IntList.list(1, 2, 3, 4, 5, 6);
+	  assertEquals(IntList.dcatenate(A, B), C);
+	  assertEquals(A, D);
+	  assertEquals(B, E);
+	}	
+    public static void main(String... args) {
+        jh61b.junit.textui.runClasses(IntListTest.class);
+    }
+}   
